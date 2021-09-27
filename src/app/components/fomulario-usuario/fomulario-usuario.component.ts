16fbbc34cdcf2531a3428f965dcbd59838f49250
David <davidcalderon700@gmail.com>
2021-09-27 16:07:59 -0600

    Problemas en la validación
    Hay problemas en la validación en el modulo de usuario.

--- src/app/components/fomulario-usuario/fomulario-usuario.component.ts
+++ src/app/components/fomulario-usuario/fomulario-usuario.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {MatDialogRef} from "@angular/material/dialog";
 import {ApiService} from "../../services/api.service";
 import {ActivatedRoute} from "@angular/router";
@@ -31,20 +31,59 @@ export class FomularioUsuarioComponent implements OnInit {
   ngOnInit(): void {
     this.route.url.subscribe(res=>console.log(res[0].path))
     this.form = new FormGroup({
-      nombre: new FormControl(this.dataUsuario !== undefined ? this.dataUsuario['nombre_usuario'] || '' : ''),
-      apellido_paterno: new FormControl(this.dataUsuario !== undefined ? this.dataUsuario['apellido_paterno'] || '' : ''),
-      apellido_materno: new FormControl(this.dataUsuario !== undefined ? this.dataUsuario['apellido_materno'] || '' : ''),
-      telefono: new FormControl(this.dataUsuario !== undefined ? this.dataUsuario['telefono_usuario'] || '' : ''),
-      sucursal_id: new FormControl(this.dataUsuario !== undefined ? this.dataUsuario['sucursal_id'] || '' : ''),
-      email: new FormControl(this.dataUsuario !== undefined ? this.dataUsuario['email'] || '' : ''),
-      password: new FormControl(this.dataUsuario !== undefined ? this.dataUsuario['password'] || '' : '')
+      nombre: new FormControl(this.dataUsuario !== undefined ? this.dataUsuario['nombre_usuario'] || '' : '', [Validators.required, Validators.pattern('^[a-zA-ZáéíóúñÑÁÉÍÓÚ ]*$')]),
+      apellido_paterno: new FormControl(this.dataUsuario !== undefined ? this.dataUsuario['apellido_paterno'] || '' : '', [Validators.required, Validators.pattern('^[a-zA-ZáéíóúñÑÁÉÍÓÚ ]*$')]),
+      apellido_materno: new FormControl(this.dataUsuario !== undefined ? this.dataUsuario['apellido_materno'] || '' : '', [Validators.required, Validators.pattern('^[a-zA-ZáéíóúñÑÁÉÍÓÚ ]*$')]),
+      telefono: new FormControl(this.dataUsuario !== undefined ? this.dataUsuario['telefono_usuario'] || '' : '', [Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern('^[0-9]*$')]),
+      sucursal_id: new FormControl(this.dataUsuario !== undefined ? this.dataUsuario['sucursal_id'] || '' : '', [Validators.required]),
+      email: new FormControl(this.dataUsuario !== undefined ? this.dataUsuario['email'] || '' : '', [Validators.required, Validators.email]),
+      password: new FormControl(this.dataUsuario !== undefined ? this.dataUsuario['password'] || '' : '',[Validators.required, Validators.minLength(8), Validators.maxLength(15)])
     })
   }
 
+  errores(atributo: string){
+    //console.log(this.form.controls.nombre.errors)
+    //console.log(this.form.controls[atributo].errors)
+    let aux = this.form.controls[atributo].errors;
+    if(aux!==null){
+      if(aux.hasOwnProperty('required')){
+        return `El campo ${atributo} es obligatorio`;
+      }
+      if(aux.hasOwnProperty('pattern') && atributo==='telefono'){
+        return `El campo ${atributo} solo acepta números`;
+      }
+      if((aux.hasOwnProperty('minlength') || aux.hasOwnProperty('maxlength')) && atributo==='telefono'){
+        return `El campo ${atributo} debe tener 10 caracteres`;
+      }
+      if(aux.hasOwnProperty('pattern')){
+        return `El campo ${atributo} solo acepta letras mayúsculas y minusculas`;
+      }
+      if(aux.hasOwnProperty('minlength') && atributo==='password'){
+        return `La contraseña debe contener de 8 a 15 caracteres`;
+      }
+      if(aux.hasOwnProperty('maxlength') && atributo==='password'){
+        return `La contraseña debe contener de 8 a 15 caracteres`;
+      }
+      if(aux.hasOwnProperty('email')){
+        return `El campo ${atributo} debe contener un email válido`;
+      }
+
+    }
+    //if(this.form.controls[atributo].errors=='required'){
+    //  return "Requeridooooo"
+    //}
+    return ""
+  }
   enviarFormulario() {
-    console.log("Enviar formulario-sucursal en el componenteb individual formulario-sucursal")
-    console.log(this.dataUsuario)
+    console.log("Enviando formulario...")
     console.log(this.form)
+    if(this.form.invalid){
+      console.log("Formulario inválido")
+      return;
+    }
+    console.log("Enviar formulario-sucursal en el componente individual formulario-sucursal")
+    console.log(this.dataUsuario)
+
     let aux = this.form.value;
     console.log(aux)
     let newUsuarioWtihoutPass : Usuario = {
@@ -70,11 +109,11 @@ export class FomularioUsuarioComponent implements OnInit {
         console.log(err)
       })
     } else {
-
       this.data.saveUsuario(newUsuarioWithPassword).toPromise().then(res => {
         console.log(res)
         console.log(this.form)
         this.form.reset();
+        this.form.updateValueAndValidity();
       }).catch(err => {
         console.log("imprimiendo la respuesta del error al crear", err.status)
         this.snack.open("Error. Por favor asegurate de que la sucursal asignada existe!","Ok!")
